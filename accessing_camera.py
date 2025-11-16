de607d8fa1b99e4759888c5bc884dec9e91325af
AmitKarnam <amitkarnam01@gmail.com>
2025-11-16 13:10:02 +0530

    added live image thresholding

--- accessing_camera.py
+++ accessing_camera.py
@@ -14,13 +14,22 @@ def access_camera():
             print("Error: Error reading video stream feed")
             break
 
-        cv2.imshow("Video Stream Feed",frame)
+        greyFrame = cv2.cvtColor(frame,cv2.COLOR_RGB2GRAY)
+
+        meanThreshFrame = cv2.adaptiveThreshold(greyFrame,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,11,2)
+
+        gaussianThreshFrame = cv2.adaptiveThreshold(greyFrame,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+
+        cv2.imshow("Video Stream Feed Mean Thresholding",meanThreshFrame)
+        cv2.imshow("Video Stream Feed Gaussian Thresholding", gaussianThreshFrame)
+
+
 
         # Press 'q' to exit
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-
+            
 
     web_cam_feed.release()
     cv2.destroyAllWindows()
