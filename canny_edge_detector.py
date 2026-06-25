0948c7d62837ee7854c214fc7065b35272ab8e4b
AmitKarnam <amitkarnam01@gmail.com>
2026-06-25 21:22:03 +0530

    Implement Canny edge detection with Sobel convolution and non-maxima suppression

--- canny_edge_detector.py
+++ canny_edge_detector.py
@@ -75,6 +75,54 @@ def ComputeSobelEdges(img: np.ndarray):
 
     return gx, gy, magnitude, direction_deg
 
+def NonMaximaSupression(magnitude: np.ndarray, direction: np.ndarray):
+    # In the direction of gradient keep only the largest pixel value and convert the rest to 0
+    # if angle is 0 , check left and right
+    # if angle is 45, check top-right and bottom-left
+    # if angle is 90, check top and bottom
+    # if angle is 135, check top-left and bottom-right
+
+    rows,columns,channel = magnitude.shape
+    output = np.zeros((rows, columns, channel), dtype=np.float32)
+
+    direction = (direction + 180) % 180
+
+    for ch in range(channel):
+        for i in range(1,rows-1):
+            for j in range(1,columns-1):
+
+                angle = direction[i,j,ch]
+                current = magnitude[i,j,ch]
+
+                # if angle is 0 to 22.5 or 157.5 to 180 ( 0 degree )
+                if (0 <= angle < 22.5) or (157.5 < angle <= 180):
+                    neighbor1 = magnitude[i,j-1,ch]
+                    neighbor2 = magnitude[i,j+1,ch]
+
+                # if angle is 22.5 to 67.5 ( 45 degree )
+                if 22.5 <= angle < 67.5:
+                    neighbor1 = magnitude[i-1,j+1,ch]
+                    neighbor2 = magnitude[i+1,j-1,ch]
+
+                # if angle is 67.5 to 112.5 ( 90 degree )
+                if 67.5 <= angle < 112.5:
+                    neighbor1 = magnitude[i-1,j,ch]
+                    neighbor2 = magnitude[i+1,j,ch]
+
+                # if angle 112.5 to 157.5 ( 135 degree )
+                else:
+                    neighbor1 = magnitude[i - 1, j - 1, ch]
+                    neighbor2 = magnitude[i + 1, j + 1, ch]
+
+                if current >= neighbor1 and current >= neighbor2:
+                    output[i,j,ch] = current
+                else:
+                    output[i,j,ch] = 0
+    
+    return output
+
+                
+
 
 if __name__ == "__main__":
     originalImg = cv2.imread(imgPath)
@@ -87,8 +135,13 @@ if __name__ == "__main__":
         255
     ).astype(np.uint8)
 
+    NMS = NonMaximaSupression(magnitude,direction)
+
     cv2.imshow("Original Image", originalImg)
 
     cv2.imshow("Sobel Magnitude", magnitude_display)
+
+    cv2.imshow("Sobel + NMS", NMS.astype(np.uint8))
+    
     cv2.waitKey(0)
     cv2.destroyAllWindows()
